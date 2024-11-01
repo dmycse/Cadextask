eaee53371004f486e2566628aa3b4d1583e0728f
dmycse <dmycse@gmail.com>
2024-11-01 19:28:28 +0200

    Feat(styles): set up App colors

--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -1,5 +1,6 @@
 'use client';
 
+import { blueGrey, grey } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
@@ -18,7 +19,17 @@ const theme = createTheme({
     `
     }
   },
-
+  palette: {
+    primary: {
+      main: grey[900],
+      light: blueGrey[100],
+      dark: blueGrey[400],
+    },
+    secondary: {
+      main: grey[50],
+    },
+  },
+  
   typography: {
     fontFamily: 'var(--font-inter)',
   }
