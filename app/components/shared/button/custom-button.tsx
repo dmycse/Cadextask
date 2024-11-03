cfbc95650b8578d4868b0a700f41ce2016fc933f
dmycse <dmycse@gmail.com>
2024-11-03 12:39:45 +0200

    Refactor(components): refctor custom button

--- app/components/shared/button/custom-button.tsx
+++ app/components/shared/button/custom-button.tsx
@@ -4,17 +4,24 @@ import { buttonStyles } from './button.styles';
 
 type CustomButtonProps = {
   type?: 'button' | 'submit';
+  disabled?: boolean;
   children: ReactNode;
 
 };
 
 export let CustomButton = ({
-    type = 'button', 
+    type = 'button',
+    disabled = false, 
     children
   }: CustomButtonProps) => {
 
   return (
-    <Button variant='contained' type={type} sx={type === 'button' ? buttonStyles.button : buttonStyles.submit}>
+    <Button 
+      variant='contained'
+      type={type} 
+      disabled={disabled}
+      sx={type === 'button' ? buttonStyles.button : buttonStyles.submit}
+    >
       {children}
     </Button>
   );
